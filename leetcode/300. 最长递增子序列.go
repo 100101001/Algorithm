0674378f4941b48eec6c4fa0990f439571642c01
李依璇 <liyixuan.01@bytedance.com>
2021-07-12 10:13:15 +0800

    最长递增子序列NLogN的解法

--- leetcode/300. 最长递增子序列.go
+++ leetcode/300. 最长递增子序列.go
@@ -2,7 +2,7 @@ package main
 
 // 最长递增子序列
 func lengthOfLIS(nums []int) int {
-	return lengthOfLISDP(nums)
+	return lengthOfLIPBinarySearchLeft(nums)
 }
 
 // dp[i] 是指以 nums[i]结尾的递增子序列最大长度
@@ -28,4 +28,32 @@ func lengthOfLISDP(nums []int) int {
 		}
 	}
 	return maxI
+}
+
+// patience sort，top内是最长递增子序列，二分左边界，piles是有效长度变量
+func lengthOfLIPBinarySearchLeft(nums []int) int {
+	top := make([]int, len(nums)+1)
+	var piles = 0 // 记录了当前nums有效长度
+	for i := 0; i < len(nums); i++ {
+		//
+		left, right := 0, piles
+		for left < right {
+			mid := left + (right-left)/2
+			if top[mid] == nums[i] {
+				right = mid
+			} else if top[mid] < nums[i] {
+				left = mid + 1
+			} else if top[mid] > nums[i] {
+				right = mid
+			}
+		}
+
+		//
+		if left == piles {
+			piles++
+		}
+		// 更新数组内容
+		top[left] = nums[i]
+	}
+	return piles
 }
